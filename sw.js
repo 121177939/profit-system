bddf8fff8539db68566c90ea455f9c661103a110
121177939 <912872449@qq.com>
2026-02-07 11:30:32 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "order-system-v20-20260205-fix2";
+const CACHE_NAME = "order-system-v20-20260207-fix3";
 const APP_SHELL = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png","./sw.js"];
 self.addEventListener("install",(e)=>{
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
